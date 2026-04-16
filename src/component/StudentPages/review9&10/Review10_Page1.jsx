3e3afc48acbbf9e057c82f98868d1a1f168f6ca0
waleed-alqadi <developer3@bytechjo.com>
2026-04-16 14:11:50 +0300

    review10

--- src/component/StudentPages/review9&10/Review10_Page1.jsx
+++ src/component/StudentPages/review9&10/Review10_Page1.jsx
@@ -18,7 +18,7 @@ const Review10_Page1 = ({ openPopup }) => {
           width="22"
           height="22"
           viewBox="0 0 90 90"
-          onClick={() => openPopup("exercise", { startIndex: 112 })}
+          onClick={() => openPopup("exercise", { startIndex: 100 })}
           style={{ overflow: "visible" }}
         >
           <image
@@ -39,28 +39,7 @@ const Review10_Page1 = ({ openPopup }) => {
           width="22"
           height="22"
           viewBox="0 0 90 90"
-          onClick={() => openPopup("exercise", { startIndex: 113 })}
-          style={{ overflow: "visible" }}
-        >
-          <image
-            className="svg-img"
-            href={arrowBtn}
-            x="0"
-            y="0"
-            width="90"
-            height="90"
-          />
-        </svg>
-      </div>
-      <div
-        className="click-icon-review6-page1-1 hover:scale-110 transition"
-        style={{ overflow: "visible" }}
-      >
-        <svg
-          width="22"
-          height="22"
-          viewBox="0 0 90 90"
-          onClick={() => openPopup("exercise", { startIndex: 114 })}
+          onClick={() => openPopup("exercise", { startIndex: 101 })}
           style={{ overflow: "visible" }}
         >
           <image
